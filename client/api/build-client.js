7b2ac2a01fd6f442336ce93f6c5172fd277ed1c1
Andrew James Burns <andrewburns89@gmail.com>
2020-06-25 12:56:51 +0100

    Resolved Nginx local service name issue

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -5,7 +5,8 @@ export default ({ req }) => {
     // We are on the server
 
     return axios.create({
-      baseURL: 'http://ingress-nginx.ingress-nginx.svc.cluster.local',
+      baseURL:
+        'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local', //This is the service name . namespace
       headers: req.headers,
     });
   } else {
