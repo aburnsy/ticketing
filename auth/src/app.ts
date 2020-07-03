3b1fe0a79ea2e50269a9f6d0648e960d8f16a73e
Andrew James Burns <andrewburns89@gmail.com>
2020-07-03 16:15:19 +0100

    Quick commit of files

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -8,8 +8,7 @@ import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
 import { signoutRouter } from './routes/signout';
 import { signupRouter } from './routes/signup';
-import { errorHandler } from './middlewares/error-handler';
-import { NotFoundError } from './errors/not-found-error';
+import { errorHandler, NotFoundError } from '@burnstickets/common';
 
 const app = express();
 app.set('trust proxy', true); //express is behind nginx proxy. Tell express it should trust traffic routed through proxy
