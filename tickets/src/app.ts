767fb3f33d9761e65bd0748f6bd5120f0840fd36
Andrew James Burns <andrewburns89@gmail.com>
2020-07-05 09:05:36 +0100

    Add ticketing plus test files

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -1,19 +1,28 @@
 import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
-import mongoose from 'mongoose';
 import cookieSession from 'cookie-session';
-import { errorHandler, NotFoundError } from '@burnstickets/common';
+import { errorHandler, NotFoundError, currentUser } from '@sgtickets/common';
+import { createTicketRouter } from './routes/new';
+import { showTicketRouter } from './routes/show';
+import { indexTicketRouter } from './routes/index';
+import { updateTicketRouter } from './routes/update';
 
 const app = express();
-app.set('trust proxy', true); //express is behind nginx proxy. Tell express it should trust traffic routed through proxy
+app.set('trust proxy', true);
 app.use(json());
 app.use(
   cookieSession({
-    signed: false, //This means the cookie is not encrytped
-    secure: process.env.NODE_ENV !== 'test', //https only
+    signed: false,
+    secure: process.env.NODE_ENV !== 'test',
   })
 );
+app.use(currentUser);
+
+app.use(createTicketRouter);
+app.use(showTicketRouter);
+app.use(indexTicketRouter);
+app.use(updateTicketRouter);
 
 app.all('*', async (req, res) => {
   throw new NotFoundError();
